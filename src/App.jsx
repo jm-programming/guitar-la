60d15a1b67b7753a7a7662b6c00522ebafde43fa
Juan Manuel Pineda <juanm.pineda@outlook.com>
2024-05-16 15:28:54 -0400

    calculando el carrito de compras

--- src/App.jsx
+++ src/App.jsx
@@ -6,35 +6,41 @@ import { db } from "./data/db"
 function App() {
   const [data, setData] = useState(db)
   const [cart, setCart] = useState([])
-    
 
+  function addToCart(item) {
+    const itemExists = cart.findIndex(guitar => item.id === guitar.id)
+    if (itemExists >= 0) {
+      const updatedCart = [...cart]
+      updatedCart[itemExists].quantity++
+      setCart(updatedCart)
+    } else {
+      item.quantity = 1
+      setCart([...cart, item])
+    }
+}
   return (
     <>
-    <Header />
-    <main className="container-xl mt-5">
-        <h2 className="text-center">Nuestra Colección</h2>        
-              <div className="row mt-5">
-                  {data.map((guitar) => (
-                    <>                          
-                      <Guitar                          
-                          key={ guitar.id }
-                          guitar={guitar}
-                          setCart={setCart}
-                          cart={ cart } 
-                          />                   
-                      </> 
-                 ))}
-               
-             </div>
-        
-    </main>
+      <Header cart={ cart } />
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+        <div className="row mt-5">
 
-
-    <footer className="bg-dark mt-5 py-5">
-        <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+          {data.map((guitar) => (           
+              <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={ addToCart}
+              />
+          ))}
         </div>
-    </footer>
+
+      </main>
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+      </footer>
 
     </>
   )
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,7 +1,5 @@
-export default function Guitar({ guitar , cart, setCart}) {
-    
+export default function Guitar({ guitar, addToCart }) {
     const { id, name, price, description, image } = guitar
-    
     return (
         <>
             <div className="col-md-6 col-lg-4 my-4 row align-items-center">
@@ -10,15 +8,15 @@ export default function Guitar({ guitar , cart, setCart}) {
                 </div>
                 <div className="col-8">
                     <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
-                    <p>{ description }</p>
+                    <p>{description}</p>
                     <p className="fw-black text-primary fs-3">${price}</p>
-                    <button 
+                    <button
                         type="button"
                         className="btn btn-dark w-100"
-                        onClick={() => setCart([...cart, guitar])}
+                        onClick={() => addToCart(guitar)}
                     >Agregar al Carrito</button>
                 </div>
-            </div>   
+            </div>
         </>
     )
 }
